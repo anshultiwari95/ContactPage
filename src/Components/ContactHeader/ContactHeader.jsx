de710b279df2b68fd763ff14d30a6d14c92c6950
anshultiwari95 <anshultiwari95@gmail.com>
2024-05-31 02:15:57 +0530

    Added HeaderContact

---
+++ src/Components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,23 @@
+import "./ContactHeader.css";
+
+const ContactHeader = () => {
+  return (
+    <body className="bdy">
+      <div>
+        <h1 className="title">CONTACT US</h1>
+      </div>
+      <div>
+        <p className="description">
+          LET'S CONNECT: WE'RE HERE TO HELP, AND WE'D LOVE TO HEAR FROM YOU!
+          WHETHER YOU HAVE A QUESTION, COMMENT, OR
+          <br />
+          JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM
+          OF THIS PAGE, OR
+          <br /> BY PHONE, EMAIL, OR SOCIAL MEDIA.
+        </p>
+      </div>
+    </body>
+  );
+};
+
+export default ContactHeader;
